6aae3184b495a3fe10d7d24a6986eadf493f4121
dalia <dalia@DESKTOP-O3HN788>
2021-02-25 17:17:58 +0200

    Added classes for the zoo

--- Seminar1_CTS/src/program/Main.java
+++ Seminar1_CTS/src/program/Main.java
@@ -1,10 +1,28 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Zoo z=new Zoo();
+		Giraffe giraffe=new Giraffe("Girafa1");
+		Giraffe giraffe2=new Giraffe("Girafa2");
+		
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
 
+		Zebra zebra=new Zebra("Zebra");
+		Zebra zebra2=new Zebra("Zebra2");
+		
+		z.addAnimal(zebra);
+		z.addAnimal(zebra2);
+		
+		z.feedAllAnimals();
 	}
 
 }
