ab88f03ab68a9c3b1e56de77304811d414a44397
dalia <dalia@DESKTOP-O3HN788>
2021-03-11 17:18:50 +0200

    Added Proiect class and modified Aplicant class

--- Seminar2_CTS/src/ro/ase/cts/clase/Angajat.java
+++ Seminar2_CTS/src/ro/ase/cts/clase/Angajat.java
@@ -7,6 +7,13 @@ public class Angajat extends Aplicant{
 	private int salariu;
 	private static int sumaFinantare=10;
 	
+	
+	public static int getSumaFinantare() {
+		return sumaFinantare;
+	}
+	public static void setSumaFinantare(int sumaFinantare) {
+		Angajat.sumaFinantare = sumaFinantare;
+	}
 	public String getOcupatie() {
 		return ocupatie;
 	}
@@ -32,9 +39,11 @@ public class Angajat extends Aplicant{
 	
 	@Override
 	public String toString() {
-		return "Angajat: Nume=" + nume + ", Prenume=" + prenume
-				+ ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte + ", DenumireProiect="
-				+ Arrays.toString(denumireProiect) + "Ocupatie=" + ocupatie + ", salariu=" + salariu;
+		StringBuilder builder=new StringBuilder("Angajat: ");
+		builder.append(super.toString());
+		builder.append("Ocupatie"+ocupatie);
+		builder.append(", Salariu="+salariu);
+		return builder.toString();
 	}
 	
 	
--- Seminar2_CTS/src/ro/ase/cts/program/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/program/Program.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Proiect;
+import ro.ase.cts.clase.Student;
 import ro.ase.cts.clase.reader.AngajatReader;
 import ro.ase.cts.clase.reader.AplicantReader;
 import ro.ase.cts.program.Program;
@@ -15,13 +17,17 @@ public class Program {
 	}
 	public static void main(String[] args) {
 		List<Aplicant> listaAngajati;
-		
+		System.out.println(Angajat.getSumaFinantare());
+		System.out.println(Student.getSumaFinantare());
+		Proiect proiectInitial=new Proiect(80);
 		try {
+			
 			listaAngajati = citesteAplicanti(new AngajatReader("angajati.txt"));
 			for(Aplicant angajat:listaAngajati)
 			{
 				System.out.println(angajat.toString());
 				System.out.println("Angajatul are finantarea: "+angajat.getFinantare());
+				angajat.afisareStatutPeProiect(proiectInitial);
 			}
 
 		} catch (FileNotFoundException e) {
