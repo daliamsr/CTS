004f2d344e9b756e1d443e505dd75e3cfe2ae3ff
dalia <dalia@DESKTOP-O3HN788>
2021-03-25 16:17:19 +0200

    Initial commit

---
+++ Seminar5/src/ro/ase/cts/SimpleFactory/clase/PersonalSpital.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.SimpleFactory.clase;
+
+public abstract class PersonalSpital {
+	public String nume;
+
+	
+	public String getNume() {
+		return nume;
+	}
+
+
+	public PersonalSpital(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+
+}
---
+++ Seminar5/src/ro/ase/cts/SimpleFactory/program/Main.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.SimpleFactory.program;
+
+import ro.ase.cts.SimpleFactory.clase.FactoryPersonal;
+import ro.ase.cts.SimpleFactory.clase.Medic;
+import ro.ase.cts.SimpleFactory.clase.PersonalSpital;
+import ro.ase.cts.SimpleFactory.clase.TipPersonal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		FactoryPersonal factory=new FactoryPersonal();
+		PersonalSpital medic=factory.getPersonal(TipPersonal.MEDIC, "Ana");
+		PersonalSpital asistent=factory.getPersonal(TipPersonal.ASISTENT,"Catalin");
+		
+		System.out.println(medic.toString());
+		System.out.println(asistent.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
