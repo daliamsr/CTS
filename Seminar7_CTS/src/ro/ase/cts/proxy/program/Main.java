92dba03a0baac3b5b64e78fc4b6f7cc91d1cdac5
dalia <dalia@DESKTOP-O3HN788>
2021-04-08 16:54:26 +0300

    Add Proxy

---
+++ Seminar7_CTS/src/ro/ase/cts/proxy/program/Main.java
@@ -0,0 +1,21 @@
+package ro.ase.cts.proxy.program;
+
+
+
+import ro.ase.cts.proxy.clase.Credit;
+import ro.ase.cts.proxy.clase.Proxy;
+import ro.ase.cts.proxy.clase.TipMoneda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Credit credit=new Credit();
+		credit.OferaCredit(TipMoneda.EUR, 100);
+		
+		Proxy proxy=new Proxy(credit);
+		proxy.OferaCredit(TipMoneda.EUR, 100);
+		proxy.OferaCredit(TipMoneda.RON, 1000);
+
+	}
+
+}
