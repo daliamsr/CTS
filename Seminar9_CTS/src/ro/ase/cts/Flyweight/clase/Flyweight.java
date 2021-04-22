c4ddf64de92fbfbd09bd928ffd9e35e83d0814ef
dalia <dalia@DESKTOP-O3HN788>
2021-04-22 16:18:28 +0300

    Initial commit

---
+++ Seminar9_CTS/src/ro/ase/cts/Flyweight/clase/Flyweight.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.Flyweight.clase;
+
+public interface Flyweight {
+
+	void printeazaRezervare(Rezervare rezervare);
+}
---
+++ Seminar9_CTS/src/ro/ase/cts/Flyweight/clase/Rezervare.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.Flyweight.clase;
+
+public class Rezervare {
+	private int nrMasa;
+	private int nrPers;
+	private int oraRezervare;
+	public Rezervare(int nrMasa, int nrPers, int oraRezervare) {
+		super();
+		this.nrMasa = nrMasa;
+		this.nrPers = nrPers;
+		this.oraRezervare = oraRezervare;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Rezervare [nrMasa=");
+		builder.append(nrMasa);
+		builder.append(", nrPers=");
+		builder.append(nrPers);
+		builder.append(", oraRezervare=");
+		builder.append(oraRezervare);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
