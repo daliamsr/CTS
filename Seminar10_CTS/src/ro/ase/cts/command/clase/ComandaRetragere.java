731aded2d3ba4e8bd1276307660354d3033f1745
dalia <dalia@DESKTOP-O3HN788.home>
2021-05-06 16:09:34 +0300

    Initial commit

---
+++ Seminar10_CTS/src/ro/ase/cts/command/clase/ComandaRetragere.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.command.clase;
+
+public class ComandaRetragere extends Comanda {
+
+	public ComandaRetragere(ContBancar cont, float suma) {
+		super(cont, suma);
+	}
+
+	@Override
+	public void executa() {
+		super.getCont().retragere(super.getSuma());
+		
+		
+	}
+
+	
+}
---
+++ Seminar10_CTS/src/ro/ase/cts/command/clase/ContBancar.java
@@ -0,0 +1,28 @@
+package ro.ase.cts.command.clase;
+
+public class ContBancar {
+
+	private String detinator;
+	private float sold;
+	public ContBancar(String detinator) {
+		super();
+		this.detinator = detinator;
+	}
+	
+	public void constituire(float suma) {
+		sold=suma;
+		System.out.println("S-a constituit contul cu suma "+suma);
+	}
+	public void retragere(float suma) {
+		if(suma<=sold) {
+			sold-=suma;
+			System.out.println("S-a retras suma de "+suma);
+		} else {
+			System.out.println("Fonduri insuficiente!");
+		}
+	}
+	public void depunere(float suma) {
+		sold+=suma;
+		System.out.println("S-a depus suma "+suma);
+	}
+}
